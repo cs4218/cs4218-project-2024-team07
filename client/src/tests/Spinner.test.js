e7cf8bd3b2a75db14161c68793e2bda54e081fd7
Eric Lee Ying Yao <ericleeyy.78@gmail.com>
2024-08-31 20:40:43 +0800

    feat: add preliminary spinner test case for client, init client jest

---
+++ client/src/tests/Spinner.test.js
@@ -0,0 +1,82 @@
+import { render, screen, act } from '@testing-library/react';
+import Spinner from '../components/Spinner';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+/**
+ * 1) Checking spinner every second for 3 seconds
+ * 2) Check login call
+ * 3) Check custom call
+ */
+
+const mockNavigate = jest.fn();
+const mockLocation = { pathname: '/test', search: '', hash: '', state: null };
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate,
+  useLocation: () => mockLocation,
+}));
+
+describe('Spinner component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers(); 
+  });
+
+  it('renders spinner with countdown', () => {
+    render(
+      <Router>
+        <Spinner/>
+      </Router>
+    );
+
+    expect(screen.getByText(/redirecting to you in 3 second/i)).toBeInTheDocument();
+    expect(screen.getByRole('status')).toBeInTheDocument();
+
+  });
+
+  it('should decrement the countdown every second', () => {
+    jest.useFakeTimers();
+    render(
+      <Router>
+        <Spinner/>
+      </Router>
+    );
+
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+    expect(screen.getByText(/redirecting to you in 2 second/i)).toBeInTheDocument();
+
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+    expect(screen.getByText(/redirecting to you in 1 second/i)).toBeInTheDocument();
+  });
+
+  it('should navigate to login path after countdown', () => {
+    render(
+      <Router>
+        <Spinner/>
+      </Router>
+    );
+
+    act(() => {
+      jest.advanceTimersByTime(3000);
+    });
+    expect(mockNavigate).toHaveBeenCalledWith('/login', { state: '/test' });
+  });
+
+  it('should navigate to a custom path after countdown', () => {
+    render(
+      <Router>
+        <Spinner path="dashboard"/>
+      </Router>
+    );
+
+    act(() => {
+      jest.advanceTimersByTime(3000);
+    });
+    expect(mockNavigate).toHaveBeenCalledWith('/dashboard', { state: '/test' });
+  });
+});
