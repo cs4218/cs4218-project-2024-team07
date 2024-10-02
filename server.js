3f45f11e47b91d80e1eb46472a7e1ac02ce487e9
loyhongshenggg <e0725346@u.nus.edu>
2024-10-02 22:36:55 +0800

    in memory db

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import connectDB from "./config/db.js";
+import { connectDB } from "./config/db.js";
 import authRoutes from './routes/authRoute.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
@@ -12,7 +12,10 @@ import cors from "cors";
 dotenv.config();
 
 //database config
-connectDB();
+// changed to implement in memory db for unit test
+if (process.env.NODE_ENV !== 'test') {
+    connectDB();
+}
 
 const app = express();
 
