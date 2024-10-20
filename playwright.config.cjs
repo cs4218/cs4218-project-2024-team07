27f2a0819f2926e1c1d308bdbe1a0395b5c81bd0
benjytan45678 <Benjytan45678@gmail.com>
2024-10-20 23:29:14 +0800

    Configure number of playwright workers to 1 to avoid race condition

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -19,7 +19,7 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -48,6 +48,8 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Safari'] },
     },
 
+    ],
+
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
@@ -67,13 +69,12 @@ module.exports = defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
 
