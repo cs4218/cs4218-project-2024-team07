467900cd7de6ea21cd68f272daa8c8d27af4b6c8
samuelcheongws <samuelcheong2000@gmail.com>
2024-10-21 13:55:51 +0800

    add jmeter command to package.json

--- jmeter/scripts/initial_load_test.sh
+++ jmeter/scripts/initial_load_test.sh
@@ -1,4 +1,4 @@
 jmeter -n \
 -t ./jmeter/test-plans/initial_load_test.jmx \
 -j ./jmeter/outputs/jmeter.log \
--f -l ./jmeter/outputs/results_output.csv
+-f -l ./jmeter/outputs/initial_load_test_output.csv
