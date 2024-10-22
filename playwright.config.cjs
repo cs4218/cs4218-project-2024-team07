414f592a32e2760cdfa5b8cd9f4982019701f93e
benjytan45678 <Benjytan45678@gmail.com>
2024-10-22 13:03:16 +0800

    Start up client and server in pipeline

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -74,10 +74,10 @@ module.exports = defineConfig({
     // },
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    port : 3000,
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'npm run dev',
+  //   port : 3000,
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
 
